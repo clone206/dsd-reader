7d3e1928bbe5bfc5a29493228e27e2b3f81400e1
clone206 <clone206@local>
2025-11-24 16:17:19 -0800

    Doc updates

--- src/lib.rs
+++ src/lib.rs
@@ -9,6 +9,96 @@
 //! This planar format was chosen due to the prevalence of DSF files and the efficiency 
 //! with which it can be iterated over and processed in certain scenarios, 
 //! however it should be trivial for the implementor to convert to interleaved format if needed.
+//!
+//! ## Examples
+//! 
+//! Opening and reading a DFF file:
+//! 
+//!```no_run
+//! use std::path::PathBuf;
+//! use dsd_reader::DsdReader;
+//! 
+//! let in_path = PathBuf::from("my/music.dff");
+//! // Constructor for use with container files. DSF works the same
+//! let dsd_reader = DsdReader::from_container(in_path.clone()).unwrap();
+//! let channels_num = dsd_reader.channels_num() as usize;
+//! let dsd_iter = dsd_reader.dsd_iter().unwrap();
+//! 
+//! for (read_size, chan_bufs) in dsd_iter {
+//!     eprintln!("read_size: usize is {} bytes.", read_size);
+//!     for chan in 0..channels_num {
+//!         my_process_channel(chan, &chan_bufs[chan]);
+//!     }
+//! }
+//! 
+//! fn my_process_channel(chan: usize, chan_bytes: &[u8]) {
+//!     eprintln!("Processing channel {} with {} bytes.", chan + 1, chan_bytes.len());
+//!     // do stuff
+//! }
+//! ```
+//! 
+//! Reading from stdin:
+//!```no_run
+//! use dsd_reader::{DsdReader, Endianness, FmtType, DsdRate};
+//! 
+//! let dsd_reader = DsdReader::new(
+//!     None, // in_path: None triggers stdin reading
+//!     FmtType::Interleaved,
+//!     Endianness::MsbFirst,
+//!     DsdRate::DSD64,
+//!     4096, // A safe choice of block size for all DSD inputs
+//!     2 // Stereo
+//! ).unwrap();
+//! let channels_num = dsd_reader.channels_num() as usize;
+//! let dsd_iter = dsd_reader.dsd_iter().unwrap();
+//! 
+//! for (read_size, chan_bufs) in dsd_iter {
+//!     eprintln!("read_size: usize is {} bytes.", read_size);
+//!     for chan in 0..channels_num {
+//!         my_process_channel(chan, &chan_bufs[chan]);
+//!     }
+//! }
+//! 
+//! fn my_process_channel(chan: usize, chan_bytes: &[u8]) {
+//!     eprintln!("Processing channel {} with {} bytes.", chan + 1, chan_bytes.len());
+//!     // do stuff
+//! }
+//! ```
+//! 
+//! Reading from raw dsd file (no metadata contained within):
+//!```no_run
+//! use dsd_reader::{DsdReader, Endianness, FmtType, DsdRate};
+//! use std::path::PathBuf;
+//! 
+//! let in_path = PathBuf::from("my/raw_audio.dsd");
+//! 
+//! let dsd_reader = DsdReader::new(
+//!     Some(in_path.clone()),
+//!     FmtType::Planar,
+//!     Endianness::LsbFirst,
+//!     DsdRate::DSD128,
+//!     4096, // A safe choice of block size for all DSD inputs
+//!     1 // Mono
+//! ).unwrap();
+//! let channels_num = dsd_reader.channels_num() as usize;
+//! let dsd_iter = dsd_reader.dsd_iter().unwrap();
+//! 
+//! for (read_size, chan_bufs) in dsd_iter {
+//!     eprintln!(
+//!         "read_size: usize is {} bytes. Not guaranteed to have filled buffers.", 
+//!         read_size
+//!     );
+//!     for chan in 0..channels_num {
+//!         my_process_channel(chan, &chan_bufs[chan]);
+//!     }
+//! }
+//! 
+//! fn my_process_channel(chan: usize, chan_bytes: &[u8]) {
+//!     eprintln!("Processing channel {} with {} bytes.", chan + 1, chan_bytes.len());
+//!     // do stuff
+//! }
+//! ```
+
 
 pub mod dsd_file;
 use crate::dsd_file::{
